a234265e21ecb4fbd1f13a559d24fca98070ab73
ElkinSanchezGL <elkinsanchezramos@gmail.com>
2025-07-16 17:30:41 -0500

    Versión exportada para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+const repo = 'github-pages-test'; 
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: isGithubPages ? `/${repo}` : '',
+  assetPrefix: isGithubPages ? `/${repo}/` : '',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
