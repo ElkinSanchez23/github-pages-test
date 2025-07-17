a2e2b0bd0b4fca000627c6cc7627cf47b785e7cc
ElkinSanchez23 <elkinsanchezramos@gmail.com>
2025-07-17 10:49:29 -0500

    Setup GitHub Actions for deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-/** @type {import('next').NextConfig} */
+
 const isGithubPages = process.env.GITHUB_PAGES === 'true';
 const repo = 'github-pages-test'; 
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   basePath: isGithubPages ? `/${repo}` : '',
